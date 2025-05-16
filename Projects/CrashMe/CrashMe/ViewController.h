bb85a75681b89c95aa26ceab8465116365f67122
duanlianjie <duanlianjie@kuaishou.com>
2025-05-16 17:54:05 +0800

    feat:[Project] 新增 CrashMe 项目

---
+++ Projects/CrashMe/CrashMe/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CrashMe
+//
+//  Created by manajay on 2025/5/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Projects/CrashMe/CrashMe/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  CrashMe
+//
+//  Created by manajay on 2025/5/16.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
